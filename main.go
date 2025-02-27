984d12f2cb45b94d91055e189013483f14f5c27f
Aaron Sanchez <saarong12@gmail.com>
2025-02-26 23:36:37 -0600

    Added ENV parsing to get Sportmonks API key.

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
+
+var API_TOKEN string
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file", err)
+	}
+
+	// Set the API token.
+	API_TOKEN = os.Getenv("API_TOKEN")
+}
 
 func main() {
 	r := gin.Default()
+
 	r.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "pong",
