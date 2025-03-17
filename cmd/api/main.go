d30d232af77e4d4a3cceb67004dd83dc1d61fc78
Aaron Sanchez <saarong12@gmail.com>
2025-03-17 00:43:07 -0500

    Created query to populate the team table in the database.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,8 @@ import (
 
 	// "github.com/aaron-g-sanchez/PROJECTS/FOOTY-FIXTURES-DISCORD-BOT-API/routes"
 
+	"log"
+
 	"github.com/aaron-g-sanchez/PROJECTS/FOOTY-FIXTURES-DISCORD-BOT-API/config"
 	"github.com/aaron-g-sanchez/PROJECTS/FOOTY-FIXTURES-DISCORD-BOT-API/db"
 )
@@ -13,12 +15,18 @@ func init() {
 }
 
 func main() {
+	run()
+}
+
+func run() {
 	// TODO: Pass desired enviroment into db.Connect via command line flags.
 	database := db.Connect(config.DatabaseConfig.StagingURI)
 	defer database.Close()
 
 	// TODO: Place behind command line flags.
-	db.PopulateDB(database)
-
+	err := db.PopulateDB(database)
+	if err != nil {
+		log.Fatal("Error populating database: ", err)
+	}
 	// routes.Run(API_TOKEN)
 }
